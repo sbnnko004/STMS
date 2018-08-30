92fd409da90a1f5770c300e7c50d85d29c8c745e
Nkosi <sbnnko004@myuct.ac.za>
2018-08-30 08:09:48 +0200

    deleed all files

--- src/main/java/com/stms/util/Admin.java
+++
@@ -1,8 +0,0 @@
-package com.stms.util;
-
-public class Admin extends User {
-    public Admin (String username, String firstName, String lastName, String emailAddress){
-        super(username, firstName, lastName, emailAddress);
-    }
-
-}
